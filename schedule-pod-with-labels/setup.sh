5d9ebb45aa0ddbf8ceaa2a113a949cc40f01148e
Omkar Shelke <85251955+omkar-shelke25@users.noreply.github.com>
2025-10-24 16:46:12 +0530

    Update setup.sh

--- schedule-pod-with-labels/setup.sh
+++ schedule-pod-with-labels/setup.sh
@@ -3,6 +3,4 @@ set -euo pipefail
 
 kubectl create ns database-storage
 
-kubectl label no controlplane disktype=ssd region=east
-
-kubectl label no node01 region=east
+kubectl label no node01 disktype=ssd region=east

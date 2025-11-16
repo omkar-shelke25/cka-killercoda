1d9510ae408c05f21c352d847133547f44003e75
Omkar Shelke <85251955+omkar-shelke25@users.noreply.github.com>
2025-11-16 20:08:22 +0530

    Create index.js

---
+++ mcp-troubleshoot-deployment/index.js
@@ -0,0 +1,23 @@
+{
+  "title": "CKA: Fix Pod Scheduling Issue",
+  "description": "Debug and fix a Pending Pod by correctly.",
+  "details": {
+    "steps": [
+      {
+        "title": "Fix MCP Postman Deployment Scheduling Issue",
+        "text": "step1.md",
+        "verify": "verify.sh"
+      }
+    ],
+    "intro": {
+      "text": "intro.md",
+      "courseData": "setup.sh"
+    },
+    "finish": {
+      "text": "finish.md"
+    }
+  },
+  "backend": {
+    "imageid": "kubernetes-kubeadm-2nodes"
+  }
+}
